ee356008df4c4c727b0882b8c47f3bc5c0fc75d8
ilhan coşkun <ilhan.coskun86@hotmail.com>
2021-01-26 23:39:44 +0300

    Odev20Ocak

---
+++ Ocak20Odev5/MyDictioanry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ocak20Odev5
+{
+    class MyDictionary<Kd, Vd>
+    {
+        Kd[] keyArray;
+        Vd[] valueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new Kd[0];
+            valueArray = new Vd[0];
+        }
+
+
+        public void Add(Kd kd, Vd vd)
+        {
+            Kd[] tempKeyArray = keyArray;
+            Vd[] tempValueArray = valueArray;
+            keyArray = new Kd[keyArray.Length + 1];
+            valueArray = new Vd[valueArray.Length + 1];
+
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+
+            }
+
+            for (int j = 0; j < tempValueArray.Length; j++)
+            {
+                valueArray[j] = tempValueArray[j];
+            }
+
+
+            keyArray[keyArray.Length - 1] = kd;
+            valueArray[valueArray.Length - 1] = vd;
+
+
+        }
+
+        public Kd[] itemkey
+        {
+            get { return keyArray; }
+        }
+        public Vd[] itemvalue
+        {
+            get { return valueArray; }
+        }
+        public int Length
+        {
+            get { return keyArray.Length; }
+        }
+    }
+}
---
+++ Ocak20Odev5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ocak20Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Giriş");
+            myDictionary.Add(2, "Önsöz");
+            myDictionary.Add(3, "Andımız");
+            myDictionary.Add(4, "İstiklal Marşı");
+
+
+            for (int i = 0; i < myDictionary.Length; i++)
+            {
+
+                Console.WriteLine("sayfa " + myDictionary.itemkey[i] + " İçerik: " + myDictionary.itemvalue[i]);
+                Console.WriteLine("**********");
+
+            }
+
+        }
+    }
+}
